fc375cd11a8c3201db096d51d9e85abf0f4129eb
Alex Nichol <unixpickle@gmail.com>
2015-07-29 21:30:01 -0400

    use clean absolute paths for dependencies

--- dependencies.go
+++ dependencies.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"ioutil"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-const dependencyKeyword = "dependency "
+const dependencyKeyword = "//deps "
 
 type ScriptFile struct {
 	Path         string
@@ -18,40 +19,42 @@ func ReadScriptFile(path string) (*ScriptFile, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+	absPath = filepath.Clean(absPath)
+
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
-	
+
 	contents, err := ioutil.ReadAll(file)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	deps, err := parseDependencyComments(contents)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	res := &ScriptFile{absPath, make([]string, len(deps))}
 	for i, dep := range deps {
-		if res.Dependencies[i], err = filepath.Abs(dep); err != nil {
-			return nil, err
+		if filepath.IsAbs(dep) {
+			res.Dependencies[i] = filepath.Clean(dep)
+		} else {
+			res.Dependencies[i] = filepath.Clean(filepath.Join(filepath.Dir(absPath), dep))
 		}
 	}
 	return res, nil
 }
 
 func parseDependencyComments(script []byte) ([]string, error) {
-	scriptStr := string(script)
 	lines := strings.Split(string(script), "\n")
 	result := []string{}
-	for i, untrimmedLine := range lines {
+	for _, untrimmedLine := range lines {
 		line := strings.TrimSpace(untrimmedLine)
 		if strings.HasPrefix(line, dependencyKeyword) {
-			result = append(result, line[len(dependencyKeyword):])
+			result = append(result, strings.Split(line[len(dependencyKeyword):], " ")...)
 		}
 	}
 	return result, nil
